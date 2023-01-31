90b1c2f7f9f201103f22ec8f8f1559c7ca14e27c
yoelIgna <124042514+yoelIgna@users.noreply.github.com>
2023-01-31 12:49:37 -0300

    Add files via upload

---
+++ LogicaTP.js
@@ -0,0 +1,9 @@
+
+ 
+      document.getElementById("submit").addEventListener('click',function ()
+{
+        
+    document.getElementById("dejamail").innerHTML = "Gracias! a la brevedad recibiras notricias";
+    document.getElementById("contacto").innerHTML = "revisa tu casilla de correo";
+
+   }  ); 
